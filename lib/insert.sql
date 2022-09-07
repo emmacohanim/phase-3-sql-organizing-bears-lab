97b3e630ea7a825966d390a18057a404b9e2b4ee
Emma Cohanim <emmacohanim@gmail.com>
2022-09-06 17:11:52 -0700

    commit tree c169167b9689f627ec1caf99397066a8fcb22cab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, alive) VALUES ('Mr. Chocolate', 2, 'F', FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Rowdy', 3, 'M', TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Tabitha',1, 'F', TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Sergeant Brown', 2, 'M', FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Melissa', 3, 'F', TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Grinch', 4, 'M', TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Wendy', 5, 'F', TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES (NULL, 1, 'F', FALSE);
